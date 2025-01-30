145de386cc9b2ce565c6cd76524248e594eb61d9
Arvind <72453093+rawata2@users.noreply.github.com>
2025-01-30 19:07:13 +0000

    begin

---
+++ main.py
@@ -0,0 +1,20 @@
+# Python 1
+# Basic Data Types
+
+h = "hello"
+w = "world"
+
+print(h,w)
+print(h+w)
+
+n1 = 5
+n2 = 4
+print(h+str(n1))
+
+multilinestring =
+""""
+str() - cast to string
+int() - cast to integer
+float() - cast to float
+
+"""
